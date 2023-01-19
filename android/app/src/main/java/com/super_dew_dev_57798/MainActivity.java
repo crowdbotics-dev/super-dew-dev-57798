a376cdd3e2ad6152a4a5ac0ce094aa153a8b3370
Crowdbotics <engineering@crowdbotics.com>
2023-01-19 00:08:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/super_dew_dev_57798/MainActivity.java
@@ -0,0 +1,15 @@
+package com.super_dew_dev_57798;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "super_dew_dev_57798";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-dew-dev-57798",
+  url: "https://super-dew-dev-57798.botics.co",
+  api: "https://super-dew-dev-57798.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
